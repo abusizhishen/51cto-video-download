d0951de634cd984bc621a63809c778acf3db8c2b
wanghongwei <abusizhishen@gmail.com>
2019-03-31 02:10:40 +0800

    修复了51cto接口更改导致的无法下载

--- cto/wejob.py
+++ cto/wejob.py
@@ -9,18 +9,18 @@ sys.setdefaultencoding('utf-8')
 class Wejob(object,):
     action = ('info', 'download')
 
-    def __init__(self, train_id, path = '学习'):
-        login = Login()
-        self.session = login.login()
-        self.train_id = train_id
+    def __init__(self, session, path='学习'):
+        self.session = session
+        self.train_id = 0
         current_path = os.path.abspath(__file__)
         self.path = os.path.abspath(os.path.join(current_path,'../../../',path))
 
-    def train(self):
+    def train(self, train_id):
+        self.train_id = train_id
         train_start = time.time()
         train = self.get_train_info()
         train_name = train['name']
-        self.path+='/'+train_name
+        self.path += '/'+train_name
         cto.check_or_make_dir(self.path)
 
         print('微职位名称:'+train_name+'\n获取课程列表')
@@ -28,7 +28,7 @@ class Wejob(object,):
 
         total_course = len(courses)
         print('总计%d门course' % (total_course))
-        #打印course名称
+        # 打印course名称
         for course in courses:
             print(str(courses.index(course)+1)+'.'+course['course_name'])
 
@@ -36,12 +36,12 @@ class Wejob(object,):
         if action == 'n':
             print '终止下载,程序退出'
             exit()
-        #保留train index page
+        # 保留train index page
         url = 'http://edu.51cto.com/center/wejob/index/view?id=%d&force=3&orig=try' % (self.train_id)
-        resp =self.session.get(url)
+        resp = self.session.get(url)
         with open(self.path+'/index.html', 'ab') as f:
             f.write(resp.content)
-        #将课程信息保存到json中去
+        # 将课程信息保存到json中去
         with open(self.path +'/info.json','w') as f:
             data = json.dumps(courses, ensure_ascii=False, indent=2)
             f.write(data)
@@ -80,25 +80,30 @@ class Wejob(object,):
         print 'train下载用时总计'+cto.total_time(time.time()-train_start)
 
     def get_download_url(self,lesson_id,video_id):
-        url = 'http://edu.51cto.com//center/player/play/m3u8?lesson_id=%s&id=%d&dp=high&type=wejoboutcourse&lesson_type=course'\
+        url = 'http://edu.51cto.com//center/player/play/m3u8?lesson_id=%s&id=%d&dp=high&type=wejoboutcourse&' \
+              'lesson_type=course'\
               %(lesson_id,video_id)
         res = self.session.get(url).text
-        return re.findall(r'https.*',res)
+        return re.findall(r'https.*', res)
 
     def get_course_info(self, course_id):
         infos = []
         current_page = 1
         while(current_page):
-            url = 'http://edu.51cto.com/center/wejob/usr/course-infoajax?train_id=%d&train_course_id=%d&page=%d&size=20'\
-                  % (self.train_id, course_id,current_page)
+            url = 'https://edu.51cto.com/center/wejob/user/course-info-ajax?&train_course_id=%d&page=%d&size=20' % (course_id,current_page)
             res = self.session.get(url).text
             data = json.loads(res)['data']
             current_page = data['current_page'] + 1 if data['current_page'] < data['count_page'] else 0
-            pages = data['data']
+            pages = data['data'][0]['list']
 
+            # 判断list里的数据是list还是dict
+            f = lambda m, pages: pages[m] if type(pages) is dict else m
             for m in pages:
+                m = f(m, pages)
+                lesson_name = m[u'lesson_name']
+                lesson_name = cto.filename_reg_check(lesson_name)
                 info = {
-                    'lesson_name': cto.filename_reg_check(m['lesson_name']),
+                    'lesson_name': lesson_name,
                     'lesson_id': m['lesson_id'],
                     'video_id': m['video_id']
                 }
@@ -106,18 +111,19 @@ class Wejob(object,):
         return infos
 
     def get_train_info(self):
-        train = {'name':self.get_train_name(),'courses':[]}
+        train = {'name': self.get_train_name(), 'courses': []}
         current_page = 1
 
         while (current_page):
-            url = 'http://edu.51cto.com/center/wejob/usr/courseajax?train_id=%d&page=%d&size=1000'%\
-                  (self.train_id,current_page)
+            url = 'https://edu.51cto.com/center/wejob/user/train-course-ajax?train_id=%d&page=%d&size=20'%\
+                  (self.train_id, current_page)
             res = self.session.get(url)
 
-            try :res = json.loads(res.text)
+            try: res = json.loads(res.text)
             except ValueError as e:
 
                 print "接口响应异常", "%s" % e
+                print res.text
                 exit()
 
             res = res['data']
@@ -125,11 +131,11 @@ class Wejob(object,):
 
             for i in res['data']:
                 course = {
-                    'course_name':cto.filename_reg_check(i['course_name'].encode('utf-8')),
-                    'train_id':i['train_id'],
-                    'train_course_id':i['train_course_id'],
-                    'lesson_num':i['lesson_num'],
-                    'number':i['number']
+                    'course_name': cto.filename_reg_check(i['course_name'].encode('utf-8')),
+                    'train_id': i['train_id'],
+                    'train_course_id': i['train_course_id'],
+                    'lesson_num': i['lesson_num'],
+                    'number': i['sort']  # 课程的序号
                 }
                 train['courses'].append(course)
         return train
@@ -148,3 +154,33 @@ class Wejob(object,):
         for i in train['courses']:
             print('微职位_id: %d; 课程名称: %s ; 总课程数: %d' % (i['train_id'],i['course_name'], i['lesson_num']))
         return
+
+    def get_train_id_by_list(self):
+        url = 'https://edu.51cto.com/center/wejob/center/trains?page=%d&size=1000'
+        resp = self.session.get(url % 1).text
+        resp_json = json.loads(resp)
+
+        data = resp_json['data']
+        if len(data) == 0:
+            exit("没有查询到微职位")
+        data = data['data']
+
+        print "以下是您购买的微职位"
+        print
+
+        for i in data:
+            print i['train_id'], i['name']
+            print
+            print "课程id: %d, 课程名称: %s" % (int(i['train_id']), i['name'])
+
+        print
+
+        while True:
+            try:
+                id = raw_input("请输入您要下载的微职位id:")
+                id = int(id)
+            except ValueError:
+                print "无效的输入:" ,id
+            else:
+                break
+        return id
